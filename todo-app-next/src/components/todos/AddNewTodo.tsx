849712e69707162a42e5abd51e09bcc2a64f7034
fattybun <fattybun1128@gmail.com>
2025-03-13 18:10:42 +0800

    refactor: extract add to do ui

---
+++ todo-app-next/src/components/todos/AddNewTodo.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import postTodo from "@/api/postTodo";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { useState } from "react";
+
+const AddNewTodo = () => {
+    const [task, setTask] = useState<string>('');
+
+    const handleAddTask = async (task: string) => {
+        console.log(task)
+        await postTodo({
+          task,
+          is_complete: false,
+          inserted_at: new Date(),
+        });
+      }
+
+    return (
+        <div className="flex gap-2 mb-4">
+            <Input type="text" placeholder="Add new task" value={task} onChange={(e) => setTask(e.target.value)} />
+            <Button className="w-24 cursor-pointer" onClick={() => handleAddTask(task)}>Add Task</Button>
+        </div>
+    )
+}
+
+export default AddNewTodo;
